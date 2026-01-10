32432b847657a9f8b58d47031363229e81513805
Leshukova Elena <e-redina@ya.ru>
2026-01-10 17:58:37 +0300

    filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,26 @@
+def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    data: Список словарей с данными о транзакциях.
+    state: Строка для фильтрации (по умолчанию 'EXECUTED').
+    return: Новый список словарей, у которых ключ state
+ соответствует указанному значению
+    """
+    return [item for item in data if item.get('state') == state]
+
+
+# Пример использования:
+if __name__ == "__main__":
+    data = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+    # Вызов со значением по умолчанию
+    print(filter_by_state(data))
+
+    # Вызов с параметром 'CANCELED'
+    print(filter_by_state(data, 'CANCELED'))
