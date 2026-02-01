2ed6416408b77a3cabbb9d8221e2ca83e7a55eca
Leshukova Elena <e-redina@ya.ru>
2026-02-01 05:56:05 +0300

    доработанный модуль маскс и тесты

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,11 @@ def get_mask_card_number(card_number: str) -> str:
     """Принимает номер карты и возвращает его маску в формате XXXX XX** **** XXXX"""
 
     card_str = str(card_number)
+    if not card_str.isdigit():
+        raise ValueError("Номер карты должен содержать только цифры")
+    if len(card_str) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
+
     first_6 = card_str[:6]
     last_4 = card_str[-4:]
 
@@ -11,14 +16,16 @@ def get_mask_card_number(card_number: str) -> str:
     return formatted_mask
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))  # Вывод: 7000 79** **** 6361
-
 
 def get_mask_account(account_number: str) -> str:
     """Принимает номер счета и возвращает его маску в формате **XXXX"""
 
     account_str = str(account_number)
+    if not account_str.isdigit():
+        raise ValueError("Номер счета должен содержать только цифры")
+    if len(account_str) < 4:
+        raise ValueError("Номер счета слишком короткий")
+
     last_4_digits = account_str[-4:]
 
     return f"**{last_4_digits}"
@@ -26,3 +33,5 @@ def get_mask_account(account_number: str) -> str:
 
 if __name__ == "__main__":
     print(get_mask_account("73654108430135874305"))  # Вывод: **4305
+
+    print(get_mask_card_number("7000792289606361"))  # Вывод: 7000 79** **** 6361
---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+# Фикстура для примера номера карты
+@pytest.fixture
+def sample_card_number():
+    return "7000792289606361"
+
+# Фикстура для примера номера счета
+@pytest.fixture
+def sample_account_number():
+    return "73654108430135874305"
