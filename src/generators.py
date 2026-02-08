73aa2948b422b50fc87c6775af98aba5a424ac29
Leshukova Elena <e-redina@ya.ru>
2026-02-08 16:07:30 +0300

    модуль generators, функции filter_by_currency, transaction_descriptions, card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -34,3 +34,58 @@ usd_transactions = filter_by_currency(transactions, "USD")
 # Выводим первые две найденные транзакции
 for _ in range(2):
     print(next(usd_transactions))
+
+
+def transaction_descriptions(transactions):
+    """
+    Генератор, который возвращает описание каждой транзакции.
+    """
+    for transaction in transactions:
+        # Извлекаем описание, если ключа нет — вернется None или пустая строка
+        yield transaction.get("description", "Описание отсутствует")
+
+#  Пример использования
+
+# Список транзакций
+transactions = [
+    {"description": "Перевод организации"},
+    {"description": "Перевод со счета на счет"},
+    {"description": "Перевод со счета на счет"},
+    {"description": "Перевод с карты на карту"},
+    {"description": "Перевод организации"}
+]
+
+descriptions = transaction_descriptions(transactions)
+
+for _ in range(5):
+    try:
+        print(next(descriptions))
+    except StopIteration:
+        break # Останавливаем цикл, если транзакции закончились
+
+
+def card_number_generator(start, end):
+    for number in range(start, end + 1):
+        # 1. Превращаем число в строку
+        num_str = str(number)
+
+        # 2. Считаем, сколько нулей нужно добавить в начало
+        zeros_needed = 16 - len(num_str)
+
+        # 3. Создаем строку из нулей и соединяем с числом
+        full_card_number = ("0" * zeros_needed) + num_str
+
+        # 4. Расставляем пробелы вручную через срезы
+        formatted_card = (
+                full_card_number[0:4] + " " +
+                full_card_number[4:8] + " " +
+                full_card_number[8:12] + " " +
+                full_card_number[12:16]
+        )
+
+        yield formatted_card
+
+
+# Проверка
+for card in card_number_generator(1, 3):
+    print(card)
