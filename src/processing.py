5413c83a51ca4c918e1c91a38f59b5511602303b
Leshukova Elena <e-redina@ya.ru>
2026-03-04 13:21:48 +0300

    13.2 process_bank_search (def filter_by_query)- добавлена функция,которая будет принимать список словарей с данными о банковских операциях и строку поиска, а возвращать список словарей, у которых в описании есть данная строка. (используя библиотеку re).

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
@@ -25,23 +28,27 @@ def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
     )
 
 
+def filter_by_query(data: list[dict], search_query: str) -> list[dict]:
+    """
+    Фильтрует список словарей по наличию строки поиска в описании (description).
+    Использует регулярные выражения для гибкого поиска.
+    """
+    # Экранируем спецсимволы в запросе и включаем игнорирование регистра. Используем re.escape, чтобы если в поиске придет что-то вроде "+", программа не выдала ошибку.
+    pattern = re.compile(re.escape(search_query), re.IGNORECASE)
+
+    return [
+        item for item in data
+        if item.get('description') and pattern.search(item['description'])
+    ]
+
+
 # Пример использования:
 if __name__ == "__main__":
     data = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03', 'description': 'Перевод организации'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30', 'description': 'Перевод с карты на карту'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12', 'description': 'Оплата услуг'},
     ]
 
-    # Вызов со значением по умолчанию
-    print(filter_by_state(data))
-
-    # Вызов с параметром 'CANCELED'
-    print(filter_by_state(data, 'CANCELED'))
-
-    # Сортировка по убыванию (по умолчанию)
-    sorted_data = sort_by_date(data)
-
-    for item in sorted_data:
-        print(item)
+    # Пример поиска "перевод" (найдет и "Перевод", и "перевод")
+    print(filter_by_query(data, "перевод"))
